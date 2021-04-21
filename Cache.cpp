20dcc1d49e5cbb6feee787a38086f54f17162c15
arturias123 <52829156+arturias123@users.noreply.github.com>
2021-04-21 17:59:11 +0700

    Add files via upload

--- Cache.cpp
+++ Cache.cpp
@@ -2,39 +2,85 @@
 #include "Cache.h"
 
 Data* Cache::read(int addr) {
-	for (int i = 0; i < p; i++)
-        if (arr[i]->addr == addr)
-            return arr[i]->data;
+    if (search(root, addr)) {
+        return getData(root, addr);
+    }
     return NULL;
 }
 Elem* Cache::put(int addr, Data* cont) {
-	arr[p++] = new Elem(addr,cont,true);
+    Elem* temp = new Elem(addr, cont, true);
+    if (q->size <= 15) {
+        enqueue(q, temp);
+        root = insert(root, addr, cont, true);
+    }
+    else {
+        Elem* res = dequeue(q);
+        root = deleteNode(root, res->addr);
+        enqueue(q, temp);
+        root = insert(root, addr, cont, true);
+        return res;
+    }
     return NULL;
 }
 Elem* Cache::write(int addr, Data* cont) {
     bool found = false;
-	for (int i = 0; i < p; i++)
+	/*for (int i = 0; i < p; i++)
         if (arr[i]->addr == addr) {
             //delete arr[i]->data;
             arr[i]->data = cont;
             arr[i]->sync = false;
             found = true;
             break;
+        }*/
+
+    if (search(root, addr)) {
+        replace(root, addr, cont);
+        for (int i = q->front; i <= q->rear; i++) {
+            if (q->array[i]->addr == addr) {
+                q->array[i]->data = cont;
+                q->array[i]->sync = false;
+            }
         }
+        found = true;
+    }
     if (!found) {
-        arr[p++] = new Elem(addr,cont,false);
+      //  arr[p++] = new Elem(addr,cont,false);
+        Elem* temp = new Elem(addr, cont, false);
+        if (q->size <= 15) {
+            enqueue(q, temp);
+            root = insert(root, addr, cont, false);
+        }
+        else {
+            Elem* res = dequeue(q);
+            root = deleteNode(root, res->addr);
+            enqueue(q, temp);
+            root = insert(root, addr, cont, false);
+            return res;
+        }
     }
     return NULL;
 }
 void Cache::print() {
-	for (int i =0; i < p; i++)
-        arr[i]->print();
+	for (int i = q->front; i <= q->rear; i++)
+        q->array[i]->print();
 }
 void Cache::preOrder() {
-	for (int i =0; i < p; i++)
-        arr[i]->print();
+    printP(root);
 }
 void Cache::inOrder() {
-    for (int i =0; i < p; i++)
-        arr[i]->print();
+    printI(root);
+}
+void Cache::printI(Node* root) {
+    if (root != NULL) {
+        printI(root->left);
+        root->elem->print();
+        printI(root->right);
+    }
+}
+void Cache::printP(Node* root) {
+    if (root != NULL) {        
+        root->elem->print();
+        printP(root->left);
+        printP(root->right);
+    }
 }
--- Cache.h
+++ Cache.h
@@ -3,31 +3,12 @@
 
 #include "main.h"
 
-class Cache {
-		Elem** arr;
-		int p;
-	public:
-		Cache(int s) {
-			arr = new Elem*[s]; 
-			p = 0;
-		}
-		~Cache() {
-			delete[] arr;
-		}
-		Data* read(int addr);
-		Elem* put(int addr, Data* cont);
-		Elem* write(int addr, Data* cont);
-		void print();
-		void preOrder();
-		void inOrder();	
-};
-
 class Node {
-public: 
-	int key;
+public:
 	Node* left;
 	Node* right;
 	int height;
+	Elem* elem = new Elem(0, 0, false);
 };
 
 int max(int a, int b) {
@@ -41,9 +22,11 @@ int height(Node* N) {
 	return N->height;
 }
 
-Node* newNode(int key) {
+Node* newNode(int key, Data* cont, bool s) {
 	Node* node = new Node();
-	node->key = key;
+	node->elem->addr = key;
+	node->elem->data = cont;
+	node->elem->sync = s;
 	node->left = NULL;
 	node->right = NULL;
 	node->height = 1;
@@ -88,15 +71,15 @@ int getBalance(Node* N) {
 	return height(N->left) - height(N->right);
 }
 
-Node* insert(Node* node, int key) {
+Node* insert(Node* node, int key, Data* cont, bool s) {
 	if (node == NULL) {
-		return newNode(key);
+		return newNode(key, cont, s);
 	}
-	if (key < node->key) {
-		node->left = insert(node->left, key);	// Insert to the left
+	if (key < node->elem->addr) {
+		node->left = insert(node->left, key, cont, s);	// Insert to the left
 	}
-	else if (key > node->key) {
-		node->right = insert(node->right, key);	// Insert to the right
+	else if (key > node->elem->addr) {
+		node->right = insert(node->right, key, cont, s);	// Insert to the right
 	}
 	else return node;
 
@@ -104,21 +87,66 @@ Node* insert(Node* node, int key) {
 
 	int balance = getBalance(node);
 
-	if (balance > 1 && key < node->left->key) { // Left left case
+	if (balance > 1 && key < node->left->elem->addr) { // Left left case
 		return rightRotate(node);
 	}
-	if (balance < -1 && key > node->right->key) { // Right right case
+	if (balance < -1 && key > node->right->elem->addr) { // Right right case
 		return leftRotate(node);
 	}
-	if (balance > 1 && key > node->left->key) {	// Left right case
+	if (balance > 1 && key > node->left->elem->addr) {	// Left right case
 		node->left = leftRotate(node->left);
 		return rightRotate(node);
 	}
-	if (balance < -1 && key < node->right->key) { // Right left case
+	if (balance < -1 && key < node->right->elem->addr) { // Right left case
 		node->right = rightRotate(node->right);
 		return leftRotate(node);
 	}
 	return node;
+}
+
+bool search(Node* node, int addr) {
+	if (node != NULL) {
+		if (node->elem->addr == addr) {
+			return true;
+		}
+		else if (addr < node->elem->addr) {
+			return search(node->left, addr);
+		}
+		else {
+			return search(node->right, addr);
+		}
+	}
+	return false;
+}
+
+Data* getData(Node* node, int addr) {
+	if (node != NULL) {
+		if (node->elem->addr == addr) {
+			return node->elem->data;
+		}
+		else if (addr < node->elem->addr) {
+			return getData(node->left, addr);
+		}
+		else {
+			return getData(node->right, addr);
+		}
+	}
+	return NULL;
+}
+
+void replace(Node* node, int addr, Data* data) {
+	if (node != NULL) {
+		if (node->elem->addr == addr) {
+			node->elem->data = data;
+			node->elem->sync = false;
+		}
+		else if (addr < node->elem->addr) {
+			replace(node->left, addr, data);
+		}
+		else {
+			replace(node->right, addr, data);
+		}
+	}
 }
 
 Node* minValueNode(Node* node) {
@@ -135,31 +163,32 @@ Node* deleteNode(Node* root, int key) {
 	if (root == NULL) {
 		return root;
 	}
-	if (key < root->key) {
+	if (key < root->elem->addr) {
 		root->left = deleteNode(root->left, key);
 	}
-	else if (key > root->key) {
+	else if (key > root->elem->addr) {
 		root->right = deleteNode(root->right, key);
 	}
 	else {
 		if ((root->left == NULL) || (root->right == NULL)) {
 			Node* temp = (root->left) ? root->left : root->right;
 
-			if (temp == NULL) {
+			if (temp == NULL) { // No child
 				temp = root;
 				root = NULL;
 			}
 			else {
 				*root = *temp;
 			}
-			free(temp);
+			delete(temp->elem);
+			delete(temp);
 		}
 		else {
 			Node* temp = minValueNode(root->right);
 
-			root->key = temp->key;
+			root->elem->addr = temp->elem->addr;
 
-			root->right = deleteNode(root->right, temp->key);
+			root->right = deleteNode(root->right, temp->elem->addr);
 		}
 	}
 	if (root == NULL) {
@@ -187,20 +216,112 @@ Node* deleteNode(Node* root, int key) {
 	return root;
 }
 
-void preOrder(Node* root) {
-	if (root != NULL) {
-		cout << root->key << " ";
-		preOrder(root->left);
-		preOrder(root->right);
-	}
-}
-
 void inOrder(Node* root) {
 	if (root != NULL) {
 		inOrder(root->left);
-		cout << root->key << " ";
+		cout << root->elem->data << " ";
 		inOrder(root->right);
 	}
 }
+
+class Queue {
+public:
+	int front, rear, size;
+	unsigned capacity;
+	Elem** array;
+};
+
+// function to create a queue
+// of given capacity.
+// It initializes size of queue as 0
+Queue* createQueue(unsigned capacity)
+{
+	Queue* queue = new Queue();
+	queue->capacity = capacity;
+	queue->front = queue->size = 0;
+
+	// This is important, see the enqueue
+	queue->rear = capacity - 1;
+	queue->array = new Elem * [queue->capacity];
+	return queue;
+}
+
+// Queue is full when size
+// becomes equal to the capacity
+int isFull(Queue* queue)
+{
+	return (queue->size == queue->capacity);
+}
+
+// Queue is empty when size is 0
+int isEmpty(Queue* queue)
+{
+	return (queue->size == 0);
+}
+
+// Function to add an item to the queue.
+// It changes rear and size
+void enqueue(Queue* queue, Elem* item)
+{
+	if (isFull(queue))
+		return;
+	queue->rear = (queue->rear + 1)
+		% queue->capacity;
+	queue->array[queue->rear] = item;
+	queue->size = queue->size + 1;
+}
+
+// Function to remove an item from queue.
+// It changes front and size
+Elem* dequeue(Queue* queue)
+{
+	if (isEmpty(queue))
+		return NULL;
+	Elem* item = queue->array[queue->front];
+	queue->front = (queue->front + 1)
+		% queue->capacity;
+	queue->size = queue->size - 1;
+	return item;
+}
+
+// Function to get front of queue
+Elem* front(Queue* queue)
+{
+	if (isEmpty(queue))
+		return NULL;
+	return queue->array[queue->front];
+}
+
+// Function to get rear of queue
+Elem* rear(Queue* queue)
+{
+	if (isEmpty(queue))
+		return NULL;
+	return queue->array[queue->rear];
+}
+
+class Cache {
+		Queue* q;
+		int p;
+		Node* root;
+	public:
+		Cache(int s) {
+			q = createQueue(MAXSIZE); 
+			p = 0;
+			root = NULL;
+		}
+		~Cache() {
+			delete[] q;
+			delete[] root;
+		}
+		Data* read(int addr);
+		Elem* put(int addr, Data* cont);
+		Elem* write(int addr, Data* cont);
+		void print();
+		void preOrder();
+		void inOrder();	
+		void printP(Node* root);
+		void printI(Node* root);
+};
 
 #endif
